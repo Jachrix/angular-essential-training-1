9e0e84b507ea6063b63b983f90ebe2bbb19b12c3
Obinna Anyim <obinnanyim@gmail.com>
2020-10-29 13:01:24 +0100

    Dependency Injections and Services: Injection Token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component'; 
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form.component';
+import { lookUpListToken, lookUpLists} from './providers';
 
 @NgModule ({
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        ReactiveFormsModule
     ],
     declarations: [
         AppComponent,
@@ -20,6 +23,9 @@ import { MediaItemFormComponent } from './media-item-form.component';
         FavoriteDirective,
         CategoryListPipe,
         MediaItemFormComponent
+    ],
+    providers:[
+        {provide: lookUpListToken, useValue: lookUpLists}
     ],
     bootstrap: [
         AppComponent
--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, inject } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { MediaItemService } from './media-item.service';
+import { lookUpListToken } from './providers';
 
 @Component({
   selector: 'mw-media-item-form',
   templateUrl: './media-item-form.component.html',
   styleUrls: ['./media-item-form.component.css']
 })
-export class MediaItemFormComponent {
+export class MediaItemFormComponent implements OnInit {
+  form: FormGroup;
+  
+  constructor( private formBuilder: FormBuilder, 
+              private mediaItemService: MediaItemService, 
+              @Inject(lookUpListToken) public lookUpLists) {}
+  
+  ngOnInit(){
+    this.form = this.formBuilder.group({
+      medium: this.formBuilder.control('Movies'),
+      name: this.formBuilder.control('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      category: this.formBuilder.control(''),
+      year: this.formBuilder.control('', this.yearValidator),     
+    });
+  }
+  
+  yearValidator(control: FormControl){
+    if(control.value.trim().length === 0){
+      return null;
+    }
+    const year = parseInt(control.value, 10);
+    
+    const minYear = 1800;
+    const maxYear = 2300;
+    
+    if(year >= minYear && year <= maxYear){
+      return null;
+    }else{
+      return { year: {
+        min: minYear,
+        max: maxYear
+      } };
+    }
+    
+  }
+  
     onSubmit(mediaItem){
         console.log(mediaItem);
+        this.mediaItemService.add(mediaItem)
     }
 }
